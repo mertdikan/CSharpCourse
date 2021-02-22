2a0bbf005b79ad26ea110c58b426c08d6e3c694f
mertdikan <mert-61-ts@hotmail.com>
2021-02-23 00:43:11 +0300

    CSharpCourse

---
+++ Delegates/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates
+{
+    class Program
+    {
+        public delegate void MyDelegate();
+        public delegate void MyDelegate2(string text);
+        public delegate int MyDelegate3(int number1, int number2);
+        static void Main(string[] args)
+        {
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.SendMessage();
+            customerManager.ShowAlert();
+
+            Console.WriteLine("----------------------------------");
+
+            MyDelegate myDelegate = customerManager.SendMessage;
+            myDelegate += customerManager.ShowAlert;
+
+
+            myDelegate -= customerManager.SendMessage;
+            myDelegate();
+
+            Console.WriteLine("----------------------------------");
+            MyDelegate2 myDelegate2 = customerManager.SendMessage2;
+            myDelegate2+= customerManager.ShowAlert2;
+
+            myDelegate2("Hello");
+            Console.WriteLine("----------------------------------");
+            
+            Matematik matematik = new Matematik();
+            MyDelegate3 myDelegate3 = matematik.Topla;
+            myDelegate3 += matematik.Carp;
+            var sonuc = myDelegate3(2, 4);
+            Console.WriteLine(sonuc);
+
+            Console.ReadLine();
+        }
+    }
+
+    public class CustomerManager
+    {
+        public void SendMessage()
+        {
+            Console.WriteLine("Hello");
+        }
+
+        public void ShowAlert()
+        {
+            Console.WriteLine("Be Carefull");
+        }
+
+        public void SendMessage2(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        public void ShowAlert2(string alert)
+        {
+            Console.WriteLine(alert);
+        }
+    }
+
+    public class Matematik
+    {
+       public int Topla(int sayi1, int sayi2)
+        {
+            return sayi1 + sayi2;
+        }
+
+        public int Carp(int sayi1, int sayi2)
+        {
+            return sayi1 * sayi2;
+        }
+    }
+}
