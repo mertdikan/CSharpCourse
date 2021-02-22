65c801847ad80992036da8806dda0764ccf7cec5
mertdikan <mert-61-ts@hotmail.com>
2021-02-22 19:46:49 +0300

    CsharpCourse

---
+++ Reflection/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reflection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //DortIslem dortIslem = new DortIslem(2, 3);
+            //Console.WriteLine(dortIslem.Topla2());
+            //Console.WriteLine(dortIslem.Topla(3, 4));
+
+            var tip = typeof(DortIslem);
+
+            DortIslem dortislem = (DortIslem)Activator.CreateInstance(tip,6,7);
+            Console.WriteLine(dortislem.Topla(4, 5));
+            Console.WriteLine(dortislem.Topla2());
+
+
+
+            Console.ReadLine();
+        }
+
+        class DortIslem
+        {
+            private int _sayi1;
+            private int _sayi2;
+
+            public DortIslem(int sayi1,int sayi2)
+            {
+                _sayi1 = sayi1;
+                _sayi2 = sayi2;
+            }
+
+            public DortIslem()
+            {
+
+            }
+            public int Topla(int sayi1,int sayi2)
+            {
+                return sayi1 + sayi2;
+            }
+
+            public int Carp(int sayi1,int sayi2)
+            {
+                return sayi1 * sayi2;
+            }
+
+            public int Topla2()
+            {
+                return _sayi1 + _sayi2;
+            }
+
+            public int Carp2()
+            {
+                return _sayi1 * _sayi2;
+            }
+
+        }
+    }
+}
